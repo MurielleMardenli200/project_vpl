9033db595c0c653ac5b7fe955b7fe011bad1b818
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-11-27 17:34:26 -0500

    constant wandb

--- constants/wandb_constants.py
+++ constants/wandb_constants.py
@@ -1,11 +1,3 @@
-import wandb
-
-api = wandb.Api()
-
 WANDB_ENTITY = "SNAIL-VPL"
 WANDB_PROJECT = "RNN-Training"
-
-project_runs = api.runs(f"{WANDB_ENTITY}/{WANDB_PROJECT}")
-
-run_count = len(project_runs) + 1
-WANDB_RUN_NAME = f"run_{run_count}"
+WANDB_RUN_NAME = f"Search_run"
