f87420fbea5a34a329faa905c32d25f2f0540393
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-11-27 16:08:34 -0500

    email added

--- simple_job.sh
+++ simple_job.sh
@@ -3,4 +3,5 @@
 #SBATCH --ntasks=4               # number of MPI processes
 #SBATCH --mem-per-cpu=1024M      # memory; default unit is megabytes
 #SBATCH --time=0-00:05           # time (DD-HH:MM)
+#SBATCH --mail-user=murielle.mardenli@gmail.com
 python hyperparameter_sweep.py
