dc0eb2ebcac2df35d880cc905c9cc7302d25fefd
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-11-27 16:10:56 -0500

    account name changed

--- simple_job.sh
+++ simple_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=murielle
+#SBATCH --account=rrg-shahabkb
 #SBATCH --ntasks=4               # number of MPI processes
 #SBATCH --mem-per-cpu=1024M      # memory; default unit is megabytes
 #SBATCH --time=0-00:05           # time (DD-HH:MM)
