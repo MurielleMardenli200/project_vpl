8de5e784a15fd52714480b461e6e62d89174ca12
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-11-28 13:09:23 -0500

    sh

--- simple_job.sh
+++ simple_job.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #SBATCH --account=rrg-shahabkb
-#SBATCH --ntasks=4               # number of MPI processes
+#SBATCH --mem=60000
+#SBATCH -c 4
 #SBATCH --gpus=1
 #SBATCH --time=0-03:00           # time (DD-HH:MM)
+
 #SBATCH --mail-user=murielle.mardenli@gmail.com
 #SBATCH --mail-type=ALL
+
 #SBATCH --output=output.log
 #SBATCH --error=error.log
 
