526490ffc3bc07696bc34bd98e351e2ec675e9de
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-11-28 13:00:35 -0500

    gpu

--- simple_job.sh
+++ simple_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=rrg-shahabkb
 #SBATCH --ntasks=4               # number of MPI processes
-#SBATCH --mem-per-cpu=1024M      # memory; default unit is megabytes
-#SBATCH --time=0-01:00           # time (DD-HH:MM)
+#SBATCH --gpus=1
+#SBATCH --time=0-03:00           # time (DD-HH:MM)
 #SBATCH --mail-user=murielle.mardenli@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --output=output.log
