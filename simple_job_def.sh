ccc6feaaaa5c4b8f804f1e60778d5a8b56002add
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-11-28 15:51:10 -0500

    def sh

---
+++ simple_job_def.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=rrg-shahabkb
+#SBATCH --mem=60000
+#SBATCH -c 8
+#SBATCH --time=0-03:00           # time (DD-HH:MM)
+#SBATCH --mail-user=murielle.mardenli@gmail.com
+#SBATCH --mail-type=ALL
+
+#SBATCH --output=output.log
+#SBATCH --error=error.log
+
+python hyperparameter_sweep.py
+
+
