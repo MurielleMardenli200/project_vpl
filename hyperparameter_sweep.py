c69c7afbd76d2e0c93663c9b0c5edb6c5d2d64d3
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-11-29 16:57:41 -0500

    partial removal

--- hyperparameter_sweep.py
+++ hyperparameter_sweep.py
@@ -71,71 +71,69 @@ def train(config):
     total_iterations = 0
     total_loss = []
 
-    # for epoch in range(config["num_epochs"]):
-    #     epoch_losses = []
-    #     for batch_idx, (sequences, labels, same_pairs) in enumerate(train_loader):
-    #         total_iterations += 1
-    #         same_pair = same_pairs[1]
-    #         num_seq = sequences.shape[1]
-    #         batch_size = sequences.shape[0]
+    for epoch in range(config["num_epochs"]):
+        epoch_losses = []
+        for batch_idx, (sequences, labels, same_pairs) in enumerate(train_loader):
+            total_iterations += 1
+            same_pair = same_pairs[1]
+            num_seq = sequences.shape[1]
+            batch_size = sequences.shape[0]
 
-    #         print(f"config {config}")
-    #         optimizer.zero_grad()
-    #         outputs, seqs = model(sequences.permute(1, 0, 2, 3, 4))
-    #         print("outputs")
-    #         print(outputs)
+            print(f"config {config}")
+            optimizer.zero_grad()
+            outputs, seqs = model(sequences.permute(1, 0, 2, 3, 4))
 
-    #         outputs_loss = outputs.reshape(num_seq, batch_size, -1)[-1]
+            outputs_loss = outputs.reshape(num_seq, batch_size, -1)[-1]
 
-    #         loss = criterion(outputs_loss.squeeze(), same_pairs)
-    #         loss.backward()
-    #         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-    #         optimizer.step()
+            loss = criterion(outputs_loss.squeeze(), same_pairs)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
 
-    #         epoch_losses.append(loss.item())
-    #         all_seqs.append(outputs.detach().cpu())
+            epoch_losses.append(loss.item())
+            all_seqs.append(outputs.detach().cpu())
 
-    #         torch_seq = torch.stack(all_seqs, dim=0)
-    #         iteration = len(all_seqs) - 1
-    #         seq = torch_seq[iteration].reshape(num_seq, batch_size, -1)
+            torch_seq = torch.stack(all_seqs, dim=0)
+            iteration = len(all_seqs) - 1
+            seq = torch_seq[iteration].reshape(num_seq, batch_size, -1)
 
-    #         # wandb.log({"batch_loss": loss.item()})
+            # wandb.log({"batch_loss": loss.item()})
 
-    #         plt.figure(figsize=(10, 6))
-    #         # for i in range(sequences.shape[0]):
-    #         #     plt.plot(
-    #         #         seq[:, i].detach().cpu().numpy(),
-    #         #         label=f"batch index {i}",
-    #         #         marker="o",
-    #         #     )
-    #         # plt.legend()
+            plt.figure(figsize=(10, 6))
+            # for i in range(sequences.shape[0]):
+            #     plt.plot(
+            #         seq[:, i].detach().cpu().numpy(),
+            #         label=f"batch index {i}",
+            #         marker="o",
+            #     )
+            # plt.legend()
 
-    #         total_loss.append(loss.item())
-    #         plt.plot(list(range(total_iterations)), total_loss)
+            # total_loss.append(loss.item())
+            # plt.plot(list(range(total_iterations)), total_loss)
 
-    #         plt.title(f"batch_loss")
-    #         plt.ylabel("Loss value")
-    #         plt.xlabel("Sequences")
-    #         hidden_size = config["hidden_size"]
-    #         batch_s = config["batch_size"]
-    #         learning_rate = config["learning_rate"]
-    #         num_layers = config["num_layers"]
-    #         plt.savefig(
-    #             f"loss_{hidden_size}_{batch_s}_{learning_rate}_{num_layers}.png"
-    #         )
-    #         plt.grid(True)
-    #         plt.close()
-    #         print(f"output for iteration {total_iterations}")
+            # plt.title(f"batch_loss")
+            # plt.ylabel("Loss value")
+            # plt.xlabel("Sequences")
+            # hidden_size = config["hidden_size"]
+            # batch_s = config["batch_size"]
+            # learning_rate = config["learning_rate"]
+            # num_layers = config["num_layers"]
+            # plt.savefig(
+            #     f"loss_{hidden_size}_{batch_s}_{learning_rate}_{num_layers}.png"
+            # )
+            # plt.grid(True)
+            # plt.close()
+            # print(f"output for iteration {total_iterations}")
 
-    #         # if batch_idx % 5 == 0:
-    #         #     wandb.log({"RNN_output_plot": wandb.Image("temp_plot.png")})
+            # # if batch_idx % 5 == 0:
+            # #     wandb.log({"RNN_output_plot": wandb.Image("temp_plot.png")})
 
-    #         current_lr = optimizer.param_groups[0]["lr"]
-    #         # wandb.log({"learning_rate": current_lr})
+            # current_lr = optimizer.param_groups[0]["lr"]
+            # wandb.log({"learning_rate": current_lr})
 
-    #     epoch_loss = np.mean(epoch_losses)
-    #     # wandb.log({"epoch_loss": epoch_loss})
-    # torch.save(model.state_dict(), "model.pth")
+        epoch_loss = np.mean(epoch_losses)
+        # wandb.log({"epoch_loss": epoch_loss})
+    torch.save(model.state_dict(), "model.pth")
     # wandb.save("model.pth")
 
 
