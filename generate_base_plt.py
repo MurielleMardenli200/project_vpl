243a4105daa321c62b62b5ef1bf8e4969016c1a7
Muriellehxh <90001299+Muriellehxh@users.noreply.github.com>
2024-12-05 19:27:36 -0500

    plt

--- generate_base_plt.py
+++ generate_base_plt.py
@@ -260,18 +260,18 @@ if __name__ == "__main__":
 
     total_iterations = 0
 
-    # for epoch in range(config["num_epochs"]):
-    #     epoch_losses = []
-    #     num_epochs = config["num_epochs"]
-    #     print(f"Epoch {epoch + 1}/{num_epochs}")
+    for epoch in range(config["num_epochs"]):
+        epoch_losses = []
+        num_epochs = config["num_epochs"]
+        print(f"Epoch {epoch + 1}/{num_epochs}")
 
-    #     for batch_idx, (sequences, labels, same_pairs) in enumerate(train_loader):
-    #         # print(f"batch idx {batch_idx}")
-    #         # print(f"sequences {sequences}")
-    #         # print(f"same pairs {same_pairs}")
-    #         same_pair = same_pairs[1]
-    #         num_seq = sequences.shape[1]
-    #         batch_size = sequences.shape[0]
+        for batch_idx, (sequences, labels, same_pairs) in enumerate(train_loader):
+            print(f"batch idx {batch_idx}")
+            print(f"sequences {sequences}")
+            print(f"same pairs {same_pairs}")
+            same_pair = same_pairs[1]
+            num_seq = sequences.shape[1]
+            batch_size = sequences.shape[0]
 
     #         optimizer.zero_grad()
     #         outputs, seqs = model(sequences.permute(1, 0, 2, 3, 4))
